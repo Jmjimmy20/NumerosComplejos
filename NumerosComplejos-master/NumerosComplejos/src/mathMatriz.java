2252fcc43e368ff05884261c391a8e9de693062a
juan felipe mora <43444251+juanmora98@users.noreply.github.com>
2019-02-13 10:16:11 -0500

    Add files via upload

--- NumerosComplejos-master/NumerosComplejos/src/mathMatriz.java
+++ NumerosComplejos-master/NumerosComplejos/src/mathMatriz.java
@@ -13,6 +13,76 @@ public class mathMatriz {
     
     
     
+    public static MatrizFilasColumnas SumaMatricial(MatrizFilasColumnas matriz1, MatrizFilasColumnas matriz2){
+        if ((matriz1.sizeFilas == matriz2.sizeFilas) && (matriz1.sizeColumnas == matriz2.sizeColumnas)){
+            ComplexNumber complejoN; 
+            MatrizFilasColumnas matrizN = new MatrizFilasColumnas(matriz1.sizeFilas, matriz1.sizeColumnas);
+            for(int i = 0; i < matriz1.sizeFilas; i ++){
+                for(int o = 0; o < matriz1.sizeColumnas; o ++){
+                    complejoN = mathComplex.SumaComplejos(matriz1.GetPosition(i, o), matriz2.GetPosition(i, o));
+                    matrizN.AddInPosition(i, o, complejoN);
+                }
+            }
+            return matrizN;
+        }
+        else{
+            System.out.println("Las matrices no son del mismo tamanio, la operacion entregara un valor null");
+            return null;
+        }
+        
+    }
+    
+    
+    public static MatrizFilasColumnas MultiplicacionScalarMatriz(MatrizFilasColumnas matriz1, ComplexNumber complejo1){
+        ComplexNumber complejoN; 
+        MatrizFilasColumnas matrizN = new MatrizFilasColumnas(matriz1.sizeFilas, matriz1.sizeColumnas);
+        for(int i = 0; i < matriz1.sizeFilas; i ++){
+            for(int o = 0; o < matriz1.sizeColumnas; o ++){
+                complejoN = mathComplex.ProductoComplejos(matriz1.GetPosition(i, o), complejo1);
+                matrizN.AddInPosition(i, o, complejoN);
+            }
+        }
+        return matrizN;
+    }
+    
+    
+    public static MatrizFilasColumnas multiplicacionMatricial(MatrizFilasColumnas matriz1, MatrizFilasColumnas matriz2){
+        if ((matriz1.sizeFilas == matriz2.sizeFilas) && (matriz1.sizeColumnas == matriz2.sizeColumnas)){
+            int contador = 1;
+            ComplexNumber complejoN; 
+            ComplexNumber complejoSumador;
+            MatrizFilasColumnas matrizN = new MatrizFilasColumnas(matriz1.sizeFilas, matriz1.sizeColumnas);
+            for(int i = 0; i < matriz1.sizeFilas; i ++){
+                System.out.println(i);
+                complejoSumador = new ComplexNumber(0, 0);
+                for(int o = 0; o < matriz1.sizeColumnas; o ++){
+                    complejoN = mathComplex.ProductoComplejos(matriz1.GetPosition(i, o), matriz2.GetPosition(o, (contador-1)));
+                    complejoSumador = mathComplex.SumaComplejos(complejoN, complejoSumador);
+                }
+                if (contador != matriz1.sizeColumnas){
+                    matrizN.AddInPosition(i, contador, complejoSumador);
+                    contador++;
+                    System.out.println(contador);
+                    i-=1;
+                }
+                else{
+                    matrizN.AddInPosition(i, contador, complejoSumador);
+                    contador = 1;
+                    System.out.println(i);
+                }
+                
+                
+            }
+            return matrizN;
+        }
+        else{
+            System.out.println("Las matrices no son del mismo tamanio, la operacion entregara un valor null");
+            return null;
+        }
+    }
+    
+    
+    
     
     public static boolean IgualMatricial(MatrizFilasColumnas matriz1, MatrizFilasColumnas matriz2){
         boolean bandera = true;
@@ -25,5 +95,9 @@ public class mathMatriz {
         }
         return bandera;
     }
+    
+    
+    
+    
     
 }
--- NumerosComplejos-master/NumerosComplejos/test/TestMatrizFIlasColumnas.java
+++ NumerosComplejos-master/NumerosComplejos/test/TestMatrizFIlasColumnas.java
@@ -39,5 +39,132 @@ public class TestMatrizFIlasColumnas {
         boolean flag = mathMatriz.IgualMatricial(matrizFinal, valorEsperado);
         assertEquals(flagEsperada, flag);
     }
+    @Test
+    public void testSumaMatricial() {
+        boolean flagEsperada = true;
+        MatrizFilasColumnas valorEsperado = new MatrizFilasColumnas(2,2);
+        ComplexNumber valor1 = new ComplexNumber(-1, 1);
+        valorEsperado.AddInPosition(0,0,valor1);
+        ComplexNumber valor2 = new ComplexNumber(-2, 2);
+        valorEsperado.AddInPosition(0,1,valor2);
+        ComplexNumber valor3 = new ComplexNumber(1, -1);
+        valorEsperado.AddInPosition(1,0,valor3);
+        ComplexNumber valor4 = new ComplexNumber(2, -2);
+        valorEsperado.AddInPosition(1,1,valor4);
+        MatrizFilasColumnas matriz1 = new MatrizFilasColumnas(2,2);
+        valor1 = new ComplexNumber(0, -1);
+        matriz1.AddInPosition(0,0,valor1);
+        valor2 = new ComplexNumber(-1, 0);
+        matriz1.AddInPosition(0,1,valor2);
+        valor3 = new ComplexNumber(-5, 0);
+        matriz1.AddInPosition(1,0,valor3);
+        valor4 = new ComplexNumber(-2, 2);
+        matriz1.AddInPosition(1,1,valor4);
+        MatrizFilasColumnas matriz2 = new MatrizFilasColumnas(2,2);
+        valor1 = new ComplexNumber(-1, 2);
+        matriz2.AddInPosition(0,0,valor1);
+        valor2 = new ComplexNumber(-1, 2);
+        matriz2.AddInPosition(0,1,valor2);
+        valor3 = new ComplexNumber(6, -1);
+        matriz2.AddInPosition(1,0,valor3);
+        valor4 = new ComplexNumber(4, -4);
+        matriz2.AddInPosition(1,1,valor4);
+        MatrizFilasColumnas matrizFinal = mathMatriz.SumaMatricial(matriz1, matriz2);
+        boolean flag = mathMatriz.IgualMatricial(matrizFinal, valorEsperado);
+        assertEquals(flagEsperada, flag);
+    }
+    
+    @Test
+    public void testMultiplicacionScalarMatrices() {
+        boolean flagEsperada = true;
+        MatrizFilasColumnas valorEsperado = new MatrizFilasColumnas(2,2);
+        ComplexNumber valor1 = new ComplexNumber(12, 21);
+        valorEsperado.AddInPosition(0,0,valor1);
+        ComplexNumber valor2 = new ComplexNumber(0, 0);
+        valorEsperado.AddInPosition(0,1,valor2);
+        ComplexNumber valor3 = new ComplexNumber(13, 13);
+        valorEsperado.AddInPosition(1,0,valor3);
+        ComplexNumber valor4 = new ComplexNumber(12, 8);
+        valorEsperado.AddInPosition(1,1,valor4);
+        MatrizFilasColumnas matriz1 = new MatrizFilasColumnas(2,2);
+        valor1 = new ComplexNumber(6, 3);
+        matriz1.AddInPosition(0,0,valor1);
+        valor2 = new ComplexNumber(0, 0);
+        matriz1.AddInPosition(0,1,valor2);
+        valor3 = new ComplexNumber(5, 1);
+        matriz1.AddInPosition(1,0,valor3);
+        valor4 = new ComplexNumber(4, 0);
+        matriz1.AddInPosition(1,1,valor4);
+        ComplexNumber complejo = new ComplexNumber(3, 2);
+        MatrizFilasColumnas matrizFinal = mathMatriz.MultiplicacionScalarMatriz(matriz1, complejo);
+        boolean flag = mathMatriz.IgualMatricial(matrizFinal, valorEsperado);
+        assertEquals(flagEsperada, flag);
+    }
+    
+    @Test
+    public void testMultiplicacionMatrices() {
+        boolean flagEsperada = true;
+        MatrizFilasColumnas valorEsperado = new MatrizFilasColumnas(3,3);
+        ComplexNumber valor1 = new ComplexNumber(26, -52);
+        valorEsperado.AddInPosition(0,0,valor1);
+        ComplexNumber valor2 = new ComplexNumber(60, 24);
+        valorEsperado.AddInPosition(0,1,valor2);
+        ComplexNumber valor3 = new ComplexNumber(26, 0);
+        valorEsperado.AddInPosition(0,2,valor3);
+        ComplexNumber valor4 = new ComplexNumber(9, 7);
+        valorEsperado.AddInPosition(1,0,valor4);
+        ComplexNumber valor5 = new ComplexNumber(1, 29);
+        valorEsperado.AddInPosition(1,1,valor5);
+        ComplexNumber valor6 = new ComplexNumber(14, 0);
+        valorEsperado.AddInPosition(1,2,valor6);
+        ComplexNumber valor7 = new ComplexNumber(48, -21);
+        valorEsperado.AddInPosition(2,0,valor7);
+        ComplexNumber valor8 = new ComplexNumber(15, 22);
+        valorEsperado.AddInPosition(2,1,valor8);
+        ComplexNumber valor9 = new ComplexNumber(20, -22);
+        valorEsperado.AddInPosition(1,0,valor9);
+        MatrizFilasColumnas matriz1 = new MatrizFilasColumnas(3,3);
+        valor1 = new ComplexNumber(3, 2);
+        matriz1.AddInPosition(0,0,valor1);
+        valor2 = new ComplexNumber(0, 0);
+        matriz1.AddInPosition(0,1,valor2);
+        valor3 = new ComplexNumber(5, -6);
+        matriz1.AddInPosition(0,2,valor3);
+        valor4 = new ComplexNumber(1, 0);
+        matriz1.AddInPosition(1,0,valor4);
+        valor5 = new ComplexNumber(4, 2);
+        matriz1.AddInPosition(1,1,valor5);
+        valor6 = new ComplexNumber(0, 1);
+        matriz1.AddInPosition(1,2,valor6);
+        valor7 = new ComplexNumber(4, -1);
+        matriz1.AddInPosition(2,0,valor7);
+        valor8 = new ComplexNumber(0, 0);
+        matriz1.AddInPosition(2,1,valor8);
+        valor9 = new ComplexNumber(4, 0);
+        matriz1.AddInPosition(1,0,valor9);
+        MatrizFilasColumnas matriz2 = new MatrizFilasColumnas(3,3);
+        valor1 = new ComplexNumber(5, 0);
+        matriz2.AddInPosition(0,0,valor1);
+        valor2 = new ComplexNumber(2, -1);
+        matriz2.AddInPosition(0,1,valor2);
+        valor3 = new ComplexNumber(6, -4);
+        matriz2.AddInPosition(0,2,valor3);
+        valor4 = new ComplexNumber(0, 0);
+        matriz2.AddInPosition(1,0,valor4);
+        valor5 = new ComplexNumber(4, 5);
+        matriz2.AddInPosition(1,1,valor5);
+        valor6 = new ComplexNumber(2, 0);
+        matriz2.AddInPosition(1,2,valor6);
+        valor7 = new ComplexNumber(7, -4);
+        matriz2.AddInPosition(2,0,valor7);
+        valor8 = new ComplexNumber(2, 7);
+        matriz2.AddInPosition(2,1,valor8);
+        valor9 = new ComplexNumber(0, 0);
+        matriz2.AddInPosition(1,0,valor9);
+        MatrizFilasColumnas matrizFinal = mathMatriz.multiplicacionMatricial(matriz1, matriz2);
+        boolean flag = mathMatriz.IgualMatricial(matrizFinal, valorEsperado);
+        assertEquals(flagEsperada, flag);
+    }
+    
     
 }
